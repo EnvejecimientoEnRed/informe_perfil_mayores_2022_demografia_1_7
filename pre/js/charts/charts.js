8c412fa0d922c760a270d6a50e1dfde6804220eb
Carlos Muñoz Díaz <47306703G@cchs.csic.es>
2022-04-07 09:05:31 +0200

    Cambios en html

--- pre/js/charts/charts.js
+++ pre/js/charts/charts.js
@@ -8,7 +8,7 @@ export function initChart(iframe) {
     //Desarrollo del gráfico > Mapa a desarrollar por Joaquín y/o Julia > No hay desarrollo en este JS
 
     //Iframe
-    setFixedIframeUrl('informe_perfil_mayores_2022_demografia_1_8','mapa_municipios_personas_mayores');
+    setFixedIframeUrl('informe_perfil_mayores_2022_demografia_1_7','mapa_municipios_personas_mayores');
 
     //Redes sociales > Antes tenemos que indicar cuál sería el texto a enviar
     setRRSSLinks('mapa_municipios_personas_mayores');
